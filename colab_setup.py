01f745d08001cb5434d3708443f3487a1f084a14
mistn <m.staniszewski001@gmail.com>
2018-03-29 04:27:01 +0200

    mount gdrive

--- colab_setup.py
+++ colab_setup.py
@@ -10,17 +10,28 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from google.colab import files
+from google.colab import auth
 
 # https://keras.io/
 os.system("pip install -q keras")
+import keras
 
-# Install the PyDrive wrapper & import libraries.
-# This only needs to be done once per notebook.
-os.system("pip install -U -q PyDrive")
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
-from google.colab import auth
+# Mount google drive
+os.system("apt-get install -y -qq software-properties-common python-software-properties module-init-tools")
+os.system("add-apt-repository -y ppa:alessandro-strada/ppa 2>&1 > /dev/null")
+os.system("apt-get update -qq 2>&1 > /dev/null")
+os.system("apt-get -y install -qq google-drive-ocamlfuse fuse")
+
+auth.authenticate_user()
 from oauth2client.client import GoogleCredentials
+creds = GoogleCredentials.get_application_default()
+import getpass
+
+os.system("google-drive-ocamlfuse -headless -id={creds.client_id} -secret={creds.client_secret} < /dev/null 2>&1 | grep URL")
+vcode = getpass.getpass()
+os.system("echo {vcode} | google-drive-ocamlfuse -headless -id={creds.client_id} -secret={creds.client_secret}")
+
 
 # Authenticate and create the PyDrive client.
 # This only needs to be done once per notebook.
@@ -29,17 +40,3 @@ def colab_auth():
     gauth = GoogleAuth()
     gauth.credentials = GoogleCredentials.get_application_default()
     drive = GoogleDrive(gauth)
-
-# Read csv from google drive to pandas dataframe
-def get_gdrive_csv(file_id):
-  get_file = drive.CreateFile({'id': file_id})
-  file_content = get_file.GetContentString()
-  df = pd.read_csv(StringIO(file_content), sep="\t")
-  return df
-
-# Write csv to google drive
-def save_gdrive_csv():
-  uploaded = drive.CreateFile({'title': 'Sample file.txt'})
-  uploaded.SetContentString('Sample upload file content')
-  uploaded.Upload()
-  print('Uploaded file with ID {}'.format(uploaded.get('id')))
