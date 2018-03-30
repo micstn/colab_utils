48e9f82294f0f9bc4bbf00d40e5fe34a50f1181c
mistn <m.staniszewski001@gmail.com>
2018-03-30 03:29:11 +0200

    Update colab_setup.py

--- colab_setup.py
+++ colab_setup.py
@@ -7,6 +7,7 @@ else:
     from io import StringIO
 
 import os
+import getpass
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -23,7 +24,6 @@ os.system("add-apt-repository -y ppa:alessandro-strada/ppa 2>&1 > /dev/null")
 os.system("apt-get update -qq 2>&1 > /dev/null")
 os.system("apt-get -y install -qq google-drive-ocamlfuse fuse")
 
-
 # Authenticate and create the PyDrive client.
 # This only needs to be done once per notebook.
 def colab_auth():  
@@ -31,3 +31,13 @@ def colab_auth():
     gauth = GoogleAuth()
     gauth.credentials = GoogleCredentials.get_application_default()
     drive = GoogleDrive(gauth)
+
+def colab_mount_gdrive():
+    auth.authenticate_user()
+    from oauth2client.client import GoogleCredentials
+    creds = GoogleCredentials.get_application_default()
+    os.system("google-drive-ocamlfuse -headless -id={creds.client_id} -secret={creds.client_secret} < /dev/null 2>&1 | grep URL")
+    vcode = getpass.getpass()
+    os.system("echo {vcode} | google-drive-ocamlfuse -headless -id={creds.client_id} -secret={creds.client_secret}")
+
+
